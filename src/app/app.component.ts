0e9eee63887586f5b8de3a3bd591575917bdc172
Martin Lapihuska <madalucard@gmail.com>
2026-01-07 18:04:44 +0100

    passing data between parent and children

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,24 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
 import type { InvestmentInput } from './investment-input.model';
+import { InvestmentResultsComponent } from './investment-results/investment-results.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
 export class AppComponent {
+  resultsData?: {
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[];
+
   calculateInvestmentResults(data: InvestmentInput) {
     //destructured data object
     const { initialInvestment, annualInvestment, expectedReturn, duration } =
@@ -34,6 +44,6 @@ export class AppComponent {
       });
     }
     console.log(annualData);
-    // return annualData;
+    this.resultsData = annualData;
   }
 }
---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css',
+})
+export class InvestmentResultsComponent {
+  // signal input
+  // result = input<{
+  //   year: number;
+  //   interest: number;
+  //   valueEndOfYear: number;
+  //   annualInvestment: number;
+  //   totalInterest: number;
+  //   totalAmountInvested: number;
+  // }[]>();
+
+  @Input() results?: {
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[];
+}
